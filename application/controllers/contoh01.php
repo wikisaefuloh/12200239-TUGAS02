acfa795fabc09be3f975613a59e545ad4f5cf35e
wikisaefuloh <wikisaefuloh11@gmail.com>
2021-10-07 15:00:30 +0700

    menambahkan framework

---
+++ application/controllers/contoh01.php
@@ -0,0 +1,13 @@
+<?php
+class contoh01 extends CI_controller
+{
+public function index()
+{
+echo"<h1>perkenalan</h1>";
+echo"Nama saya WIKI SAEFULOH
+	saya tinggal di daerah pangandaran
+	olahraga yang saya sukai adalah basket
+	no telp 081320270416":
+	}
+{
+?>
